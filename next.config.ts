31f27c5d6e757bd036a2830fd4511a2ec8ec8e0c
Rohit Gupta <rjgupta2116@gmail.com>
2025-03-05 12:32:42 +0530

    refactor: image domains changed to image.remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["via.placeholder.com", "images.unsplash.com", "source.unsplash.com", "www.everythingtalent.ai"],
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.everythingtalent.ai",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
